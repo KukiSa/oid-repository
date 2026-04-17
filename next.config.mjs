dc82d48a728cab686b21a5316203a81f67bb8424
iks <i@iks.moe>
2026-04-17 09:27:29 +0800

    fix: No output
    Signed-off-by: iks <i@iks.moe>

--- next.config.mjs
+++ next.config.mjs
@@ -7,16 +7,15 @@ const nextConfig = {
     output: 'export',
     images: {
         unoptimized: true
+    },
+    turbopack: {
+        resolveAlias: {
+            'next-mdx-import-source-file': './mdx-components.jsx'
+        }
     }
 }
 const withNextra = nextra({
 })
 
 // Export the final Next.js config with Nextra included
-export default withNextra({
-    turbopack: {
-        resolveAlias: {
-            'next-mdx-import-source-file': './mdx-components.jsx'
-        }
-    }
-})
+export default withNextra(nextConfig)
